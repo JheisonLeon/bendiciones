caada5af9b7d98220779eddea165f2b26dc3b01d
Jheison Leon <jheisonjr15@gmail.com>
2023-12-18 21:31:09 -0400

    added user

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
